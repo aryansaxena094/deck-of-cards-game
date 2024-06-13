9f3607434e7440a4fe6306b579222c7ad70569fe
Aryan <aryan.saxena094@gmail.com>
2024-06-13 14:03:04 -0400

    added Player class

---
+++ src/main/java/com/example/cards_game/model/Player.java
@@ -0,0 +1,38 @@
+package com.example.cards_game.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private String id;
+    private List<Card> hand;
+
+    public Player(String id) {
+        this.id = id;
+        hand = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void addCard(Card card){
+        hand.add(card);
+    }
+
+    public int getHandValue() {
+        int value = 0;
+        for (Card card : hand) {
+            value += card.getRank().ordinal() + 1;
+        }
+        return value;
+    }
+
+    public String toString() {
+        return "Player{" + id + ", hand=" + hand + '}';
+    }
+}
