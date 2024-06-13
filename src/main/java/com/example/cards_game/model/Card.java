3cae08543a302a0afb8e0479596d35a84c546d71
Aryan <aryan.saxena094@gmail.com>
2024-06-13 13:51:53 -0400

    Add Card class

---
+++ src/main/java/com/example/cards_game/model/Card.java
@@ -0,0 +1,31 @@
+package com.example.cards_game.model;
+
+public class Card {
+    public enum Suit {
+        HEARTS, DIAMONDS, CLUBS, SPADES
+    }
+
+    public enum Rank {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
