2223dcc46082bda99e73192a2ca3e4d333d86c35
Aryan <aryan.saxena094@gmail.com>
2024-06-13 12:53:28 -0400

    first commit

---
+++ src/test/java/com/example/cards_game/CardsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cards_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
