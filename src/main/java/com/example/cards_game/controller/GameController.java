5a452f754d22e0c906a1d9a1595fbe3d75bd2ffa
Aryan <aryan.saxena094@gmail.com>
2024-06-14 03:04:27 -0400

    Fix: Logger to Slf4j

--- src/main/java/com/example/cards_game/controller/GameController.java
+++ src/main/java/com/example/cards_game/controller/GameController.java
@@ -3,8 +3,6 @@ package com.example.cards_game.controller;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,11 +20,12 @@ import com.example.cards_game.model.Game;
 import com.example.cards_game.model.Player;
 import com.example.cards_game.service.GameService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/game")
+@Slf4j
 public class GameController {
-
-    private static final Logger logger = LoggerFactory.getLogger(GameController.class);
 
     @Autowired
     private GameService gameService;
@@ -34,42 +33,42 @@ public class GameController {
     @PostMapping
     public ResponseEntity<String> createGame() {
         String gameId = gameService.createGame();
-        logger.info("Game created with id: {}", gameId);
+        log.info("Game created with id: {}", gameId);
         return new ResponseEntity<>(gameId, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<Map<String, Game>> getAllGames() {
         Map<String, Game> games = gameService.getAllGames();
-        logger.info("All games retrieved");
+        log.info("All games retrieved");
         return new ResponseEntity<>(games, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteGame(@PathVariable String id) {
         gameService.deleteGame(id);
-        logger.info("Game deleted with id: {}", id);
+        log.info("Game deleted with id: {}", id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/{gameId}/deck")
     public ResponseEntity<String> addDeckToGame(@PathVariable String gameId) {
         String responseMessage = gameService.addDeckToGame(gameId);
-        logger.info("Deck added to game with ID: {}", gameId);
+        log.info("Deck added to game with ID: {}", gameId);
         return new ResponseEntity<>(responseMessage, HttpStatus.CREATED);
     }
 
     @PostMapping("/{gameId}/addplayer")
     public ResponseEntity<Void> addPlayerToGame(@PathVariable String gameId, @RequestBody Player player) {
         gameService.addPlayerToGame(gameId, player);
-        logger.info("Player added to game with id: {} player id: {}", gameId, player.getId());
+        log.info("Player added to game with id: {} player id: {}", gameId, player.getId());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{gameId}/player/{playerId}")
     public ResponseEntity<Void> removePlayerFromGame(@PathVariable String gameId, @PathVariable String playerId) {
         gameService.removePlayerFromGame(gameId, playerId);
-        logger.info("Player removed from game with id: {} player id: {}", gameId, playerId);
+        log.info("Player removed from game with id: {} player id: {}", gameId, playerId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
