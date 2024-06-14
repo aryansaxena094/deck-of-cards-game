c5f85ae0d23bb0e11686c0abef96e4513f78d4d8
Aryan <aryan.saxena094@gmail.com>
2024-06-13 22:01:45 -0400

    Chore: Added Event Logs in GameService

--- src/main/java/com/example/cards_game/service/GameService.java
+++ src/main/java/com/example/cards_game/service/GameService.java
@@ -18,33 +18,43 @@ public class GameService {
     @Autowired
     private ConcurrentGameStore gameStore;
 
+    @Autowired
+    private EventLogger eventLogger;
+
     public String createGame() {
         String gameId = UUID.randomUUID().toString();
         gameStore.createGame(gameId);
+        eventLogger.logEvent(gameId, "CREATE_GAME", "Game created with id: " + gameId);
         return gameId;
     }
 
     public void deleteGame(String gameId) {
         gameStore.deleteGame(gameId);
+        eventLogger.logEvent(gameId, "DELETE_GAME", "Game deleted with id: " + gameId);
     }
 
     public void addPlayerToGame(String gameId, Player player) {
         gameStore.addPlayerToGame(gameId, player);
+        eventLogger.logEvent(gameId, "ADD_PLAYER", "Player added to game with id: " + gameId + " player id: " + player.getId());
     }
 
     public void removePlayerFromGame(String gameId, String playerId) {
         gameStore.removePlayerFromGame(gameId, playerId);
+        eventLogger.logEvent(gameId, "REMOVE_PLAYER", "Player removed from game with id: " + gameId + " player id: " + playerId);
     }
 
     public void addDeckToGame(String gameId, Deck deck) {
         gameStore.addDeckToGame(gameId, deck);
+        eventLogger.logEvent(gameId, "ADD_DECK", "Deck added to game with id: " + gameId + " deck id: " + deck.getCards());
     }
 
     public Card dealCard(String gameId){
+        eventLogger.logEvent(gameId, "DEAL_CARD", "Card dealt from game with id: " + gameId);
         return gameStore.dealCard(gameId);
     }
 
     public Map<String, Game> getAllGames(){
+        eventLogger.logEvent("ALL_GAMES", "GET_ALL_GAMES", "All games retrieved");
         return gameStore.getGames();
     }
 }
