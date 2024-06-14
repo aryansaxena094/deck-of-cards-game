86d0226e3a5459e579f673f8814e00c289520464
Aryan <aryan.saxena094@gmail.com>
2024-06-13 23:50:20 -0400

    Fix: Standard deck is created from the endpoint

--- src/main/java/com/example/cards_game/model/Deck.java
+++ src/main/java/com/example/cards_game/model/Deck.java
@@ -8,31 +8,32 @@ public class Deck {
     private List<Card> cards;
 
     public Deck() {
-        cards = new ArrayList<>();
-        for(Card.Suit suit : Card.Suit.values()){
-            for(Card.Rank rank : Card.Rank.values()){
+        this.cards = new ArrayList<>();
+        initializeStandardDeck();
+    }
+
+    private void initializeStandardDeck() {
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
                 cards.add(new Card(suit, rank));
             }
         }
-    }
-    
-    public void shuffle(){
         Collections.shuffle(cards);
     }
 
-    public Card dealCard(){
-        if(cards.isEmpty()){
+    public List<Card> getCards() {
+        return cards;
+    }
+    
+    public Card dealCard() {
+        if (cards.isEmpty()) {
             return null;
         }
         return cards.remove(0);
     }
 
-    public int remainingCardsCount(){
+    public int remainingCardsCount() {
         return cards.size();
-    }
-
-    public List<Card> getCards(){
-        return cards;
     }
 
     public String toString() {
